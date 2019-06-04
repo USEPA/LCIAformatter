917b5bdc298d443a81c7f9f55d5ee9787fc094a3
WesIngwersen <ingwersen.wesley@epa.gov>
2019-06-03 23:06:38 -0400

    Correct example to use TRACI2.1 as system and supply another outputfolder

--- examples/traci.py
+++ examples/traci.py
@@ -1,13 +1,14 @@
 import logging as log
 import lciafmt
 
+outputpath = '../lcia_formatter_output/'
 
 def main():
     log.basicConfig(level=log.INFO)
     data = lciafmt.get_traci()
-    lciafmt.map_flows(data, system="TRI")
-    data.to_csv("../out/traci_2.1.csv", index=False)
-    # lciafmt.to_jsonld(data, "../out/traci_2.1_json.zip")
+    lciafmt.map_flows(data, system="TRACI2.1")
+    data.to_csv(outputpath+"traci_2.1.csv", index=False)
+    lciafmt.to_jsonld(data, outputpath+"traci_2.1_json.zip")
 
 
 if __name__ == "__main__":
