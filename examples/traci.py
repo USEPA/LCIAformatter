259458191222ea3e4ebbbc16f45176e520fb6688
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-01-23 11:48:54 -0500

    Modify TRACI script to handle the use of modified CFs

--- examples/traci.py
+++ examples/traci.py
@@ -3,6 +3,8 @@ import os
 
 import lciafmt
 
+mod = 'NETL'
+
 def main():
     modulepath = os.path.dirname(
         os.path.realpath(__file__)).replace('\\', '/')
@@ -12,12 +14,22 @@ def main():
     log.basicConfig(level=log.INFO)
     data = lciafmt.get_method(lciafmt.Method.TRACI)
     
+    if mod is not None:
+        log.info("getting modified CFs")
+        modified_cfs=lciafmt.get_modification(mod,"TRACI2.1")
+        data = data.merge(modified_cfs,how='left',on=['Flowable','Context','Indicator'])
+        data.loc[data['Updated CF'].notnull(),'Characterization Factor']=data['Updated CF']
+        data = data.drop(columns=['Updated CF'])
+        data['Method']="TRACI 2.1 (NETL mod)"
+    
     # map the flows to the Fed.LCA commons flows
     # set preserve_unmapped=True if you want to keep unmapped
     # flows in the resulting data frame
     mapped_data = lciafmt.map_flows(data, system="TRACI2.1")
 
     # write the result to JSON-LD and CSV
+    if mod is not None:
+        outputpath=outputpath+mod+"_"
     mapped_data.to_csv(outputpath+"traci_2.1.csv", index=False)
     json_pack = outputpath+"traci_2.1_json.zip"
     if os.path.exists(json_pack):
