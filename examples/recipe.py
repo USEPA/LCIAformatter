21789763cbac57c70e5799ab17fb841dd4d888b7
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-03-20 12:19:31 -0400

    updates for endpoint conversion and breakout of json by method

--- examples/recipe.py
+++ examples/recipe.py
@@ -3,6 +3,9 @@ import os
 
 import lciafmt
 
+#To obtain LCIA endpoint set to True
+apply_endpoint = True
+
 def main():
     modulepath = os.path.dirname(
         os.path.realpath(__file__)).replace('\\', '/')
@@ -10,7 +13,7 @@ def main():
     os.makedirs(outputpath, exist_ok=True)
 
     log.basicConfig(level=log.INFO)
-    data = lciafmt.get_method(lciafmt.Method.RECIPE_2016)
+    data = lciafmt.get_method(lciafmt.Method.RECIPE_2016, endpoint = apply_endpoint)
     
     #export lcia to csv before mapping
     data.to_csv(outputpath+'Recipe_source.csv', index=False)
@@ -26,10 +29,11 @@ def main():
     # write the result to JSON-LD and CSV
     for method in mapped_data['Method'].unique():
         mapped_data[mapped_data['Method']==method].to_csv(outputpath+method.replace('/','_')+".csv", index=False)
-    json_pack = outputpath+"recipe_2016_json.zip"
-    if os.path.exists(json_pack):
-        os.remove(json_pack)
-    lciafmt.to_jsonld(mapped_data, json_pack)
+        json_pack = outputpath+method.replace('/','_')+"_json.zip"
+        if os.path.exists(json_pack):
+            os.remove(json_pack)
+        data_for_json = mapped_data[mapped_data['Method']==method]
+        lciafmt.to_jsonld(data_for_json, json_pack)
 
 
 if __name__ == "__main__":
