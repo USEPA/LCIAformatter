7d8389cc350926c79a920d515874eb7175648820
Ben Young <44471635+bl-young@users.noreply.github.com>
2021-03-02 20:16:02 -0500

    apply save_json function to example files

--- examples/fedefl_inventory_methods.py
+++ examples/fedefl_inventory_methods.py
@@ -1,26 +1,20 @@
 import logging as log
-import os
 import lciafmt
 import fedelemflowlist as fedefl
-from lciafmt.util import outputpath, store_method
+from lciafmt.util import store_method, save_json
 
 method = lciafmt.Method.FEDEFL_INV
 
 def main():
 
-    os.makedirs(outputpath, exist_ok=True)
     log.basicConfig(level=log.INFO)
-    file = method.get_filename()
     log.info(fedefl.subset_list.get_subsets())
     subsets = None
 
     inventory_methods = lciafmt.get_method(method_id='FEDEFL Inventory',subset=subsets)
     
     store_method(inventory_methods, method)
-    json_pack = outputpath+file+"_json.zip"
-    if os.path.exists(json_pack):
-        os.remove(json_pack)
-    #lciafmt.to_jsonld(inventory_methods, json_pack)
+    save_json(method, inventory_methods)
     
 if __name__ == "__main__":
     main()
