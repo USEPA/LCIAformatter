184464ce85cf676a50361ad9e3007cdbeac679d0
Ben Young <44471635+bl-young@users.noreply.github.com>
2021-03-09 19:42:08 -0500

    rearrange order of accessing indicators and minor cleanup

--- lciafmt/iw.py
+++ lciafmt/iw.py
@@ -6,7 +6,7 @@ import lciafmt.df as df
 import lciafmt.util as util
 
 def get(endpoint=False, file=None, url=None) -> pandas.DataFrame:
-    # Download Access file and call read function to transfer into dataframe
+    """Download Access file and call read function to transfer into dataframe"""
     log.info("get method Impact World")
 
     # Check for drivers and display help message if absent
@@ -42,7 +42,8 @@ def get(endpoint=False, file=None, url=None) -> pandas.DataFrame:
     return df
 
 def _read(access_file: str) -> pandas.DataFrame:
-    # Read the data from the Access database with the given path into a Pandas data frame.
+    """Read the data from the Access database with the given path into a
+    Pandas data frame."""
 
     log.info("read Impact World from file %s", access_file)
 
@@ -54,11 +55,29 @@ def _read(access_file: str) -> pandas.DataFrame:
 
     cnxn = pyodbc.connect(connStr)
     crsr = cnxn.cursor()
+    records = []
 
-# List relevant sheets in Impact World Access file. Second item in tuple tells the source of compartment information.
-# Compartment for water categories are not included in access file, defined below.
-# Elementary flow names are used to define the compartment for land transformation and occupation
-# Compartment and Subcompartment data is available in the Access file for other categories.
+    # Extract non regionalized data from "CF - not regionalized - All other impact categories"
+    crsr.execute("SELECT * FROM [CF - not regionalized - All other impact categories]")
+    rows = crsr.fetchall()
+    for row in rows:
+        df.record(
+            records,
+            method="Impact World",
+            indicator = row[1],
+            indicator_unit=row[2],
+            flow=row[5],
+            flow_category=row[3] + "/" + row[4],
+            flow_unit=row[8],
+            cas_number=util.format_cas(row[6]).lstrip("0"),
+            factor=row[7])
+
+    """List relevant sheets in Impact World Access file. Second item in tuple
+    tells the source of compartment information. Compartment for water
+    categories are not included in access file, defined below. Elementary flow
+    names are used to define the compartment for land transformation and
+    occupation. Compartment and Subcompartment data is available in the Access
+    file for other categories."""
     regional_sheets = [("CF - regionalized - WaterScarc - aggregated", "Raw/in water"),
                        ("CF - regionalized - WaterAvailab_HH - aggregated", "Raw/in water"),
                        ("CF - regionalized - LandTrans - aggregated", "Elementary Flow"),
@@ -68,28 +87,10 @@ def _read(access_file: str) -> pandas.DataFrame:
                        ("CF - regionalized - AcidFW - aggregated", "Compartment"),
                        ("CF - regionalized - AcidTerr - aggregated", "Compartment"),
                        ("CF - regionalized - EutroFW - aggregated", "Compartment"),
-                       ("CF - not regionalized - All other impact categories", "Compartment")]
-
-    records = []
+                       ]
 
     for x in regional_sheets:
-        # Extract all data from Access sheet containing non-regionalized flows and write to records.
-        if x[0] == "CF - not regionalized - All other impact categories":
-            crsr.execute("SELECT * FROM [CF - not regionalized - All other impact categories]")
-            rows = crsr.fetchall()
-            for row in rows:
-                df.record(
-                    records,
-                    method="Impact World",
-                    indicator = row[1],
-                    indicator_unit=row[2],
-                    flow=row[5],
-                    flow_category=row[3] + "/" + row[4],
-                    flow_unit=row[8],
-                    cas_number=util.format_cas(row[6]).lstrip("0"),
-                    factor=row[7])
-
-        elif x[0] == "CF - regionalized - PartMatterForm - aggregated":
+        if x[0] == "CF - regionalized - PartMatterForm - aggregated":
             # Extract global flows from the particulate matter Access sheet
             # Structure of this sheet is
             sql = "SELECT * FROM [" + x[0] + "] WHERE (([" + x[0] + "].Region In('World')))"
@@ -128,9 +129,8 @@ def _read(access_file: str) -> pandas.DataFrame:
                 if {'Compartment', 'Subcompartment'}.issubset(cols):
                     category_stmt = row.Compartment + "/" + row.Subcompartment
                 elif x[0] in ['CF - regionalized - LandTrans - aggregated',
-                              'CF - regionalized - LandOcc - aggregated']:
-                    category_stmt = row.__getattribute__('Elem flow')
-                elif x[0] in ['CF - regionalized - WaterScarc - aggregated',
+                              'CF - regionalized - LandOcc - aggregated',
+                              'CF - regionalized - WaterScarc - aggregated',
                               'CF - regionalized - WaterAvailab_HH - aggregated']:
                     category_stmt = flow_stmt
                 else:
@@ -151,18 +151,26 @@ def _read(access_file: str) -> pandas.DataFrame:
 
 
 def update_context(df_context) -> pandas.DataFrame:
-
-    # replace unspecified air and water flows for impact categories that don't rely on sub-compartments for
-    # characterization factor selection.
-    single_context = ['Freshwater acidification', 'Terrestrial acidification', 'Climate change, long term',
-                      'Climate change, short term', 'Climate change, ecosystem quality, short term',
-                      'Climate change, ecosystem quality, long term', 'Climate change, human health, short term',
-                      'Climate change, human health, long term', 'Photochemical oxidant formation',
-                      'Ozone Layer Depletion', 'Ozone layer depletion','Marine acidification, short term',
-                      'Marine acidification, long term','Ionizing radiations']
+    """replaces unspecified air and water flows for impact categories that 
+    don't rely on sub-compartments for  characterization factor selection."""
+    single_context = ['Freshwater acidification',
+                      'Terrestrial acidification',
+                      'Climate change, long term',
+                      'Climate change, short term',
+                      'Climate change, ecosystem quality, short term',
+                      'Climate change, ecosystem quality, long term',
+                      'Climate change, human health, short term',
+                      'Climate change, human health, long term',
+                      'Photochemical oxidant formation',
+                      'Ozone Layer Depletion',
+                      'Ozone layer depletion',
+                      'Marine acidification, short term',
+                      'Marine acidification, long term',
+                      'Ionizing radiations',
+                      ]
 
     context = { 'Air/(unspecified)' : 'Air',
-                # 'Water/(unspecified)' : 'Water',
+               # 'Water/(unspecified)' : 'Water',
                 }
 
     df_context.loc[df_context['Indicator'].isin(single_context),
