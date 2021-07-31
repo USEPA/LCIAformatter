48903c20910c50bc56498b4d2e6b121ec8ae46bb
Ben Young <Ben.Young@erg.com>
2021-07-30 20:13:43 -0400

    refactor recipe midpoints with openpyxl

--- lciafmt/recipe.py
+++ lciafmt/recipe.py
@@ -7,7 +7,6 @@ ReCiPe model
 """
 
 import pandas as pd
-import xlrd
 import openpyxl
 
 import lciafmt.cache as cache
@@ -120,7 +119,7 @@ def _read(file: str) -> pd.DataFrame:
     log.info("read ReCiPe 2016 from file %s", file)
     wb = openpyxl.load_workbook(file, read_only = True)
     records = []
-    for name in wb:
+    for name in wb.sheetnames:
         if _eqstr(name, "Version") or _eqstr(
                 name, "Midpoint to endpoint factors"):
             continue
@@ -132,7 +131,8 @@ def _read(file: str) -> pd.DataFrame:
 def _read_endpoints(file: str) -> pd.DataFrame:
     log.info("reading endpoint factors from file %s", file)
     wb = openpyxl.load_workbook(file, read_only = True)
-    endpoint_cols = ['Method','EndpointMethod', 'EndpointIndicator', 'EndpointUnit','EndpointConversion']
+    endpoint_cols = ['Method','EndpointMethod', 'EndpointIndicator',
+                     'EndpointUnit','EndpointConversion']
     endpoint = pd.DataFrame(columns = endpoint_cols)
     endpoints = []
     perspectives = ["I", "H", "E"]
@@ -146,7 +146,7 @@ def _read_endpoints(file: str) -> pd.DataFrame:
             flow_col = 0
 
             endpoint_factor_count = 0
-            for row in range(start_row, sheet.nrows):
+            for row in range(start_row, sheet.max_row):
                 indicator = xls.cell_str(sheet, row, flow_col)
                 indicator_unit = xls.cell_str(sheet, row, flow_col+1)
                 for i in range(0, 3):
@@ -185,11 +185,11 @@ def _read_endpoints(file: str) -> pd.DataFrame:
 
 def _read_mid_points(sheet: openpyxl.worksheet.worksheet.Worksheet,
                      records: list):
-    log.debug("try to read midpoint factors from sheet %s", sheet.name)
+    log.debug("try to read midpoint factors from sheet %s", sheet.title)
 
     start_row, data_col, with_perspectives = _find_data_start(sheet)
     if start_row < 0:
-        log.debug("could not find a value column in sheet %s", sheet.name)
+        log.debug("could not find a value column in sheet %s", sheet.title)
         return
 
     flow_col = _find_flow_column(sheet)
@@ -202,44 +202,44 @@ def _read_mid_points(sheet: openpyxl.worksheet.worksheet.Worksheet,
 
     perspectives = ["I", "H", "E"]
     factor_count = 0
-    for row in range(start_row, sheet.nrows):
+    for row in sheet.iter_rows(min_row=start_row):
         if compartment_col > -1:
-            compartment = xls.cell_str(sheet, row, compartment_col)
+            compartment = xls.cell_str(row[compartment_col])
         if compartment in contexts:
             compartment = contexts[compartment]
         if unit_col > -1:
-            flow_unit = xls.cell_str(sheet, row, unit_col)
+            flow_unit = xls.cell_str(row[unit_col])
             if "/" in flow_unit:
                 flow_unit = flow_unit.split("/")[1].strip()
         cas = ""
         if cas_col > -1:
-            cas=format_cas(xls.cell_f64(sheet, row, cas_col))
+            cas=format_cas(xls.cell_f64(row[cas_col]))
 
         if with_perspectives:
             for i in range(0, 3):
-                val = xls.cell_f64(sheet, row, data_col + i)
+                val = xls.cell_f64(row[data_col + i])
                 if val == 0.0:
                     continue
                 dfutil.record(records,
                               method="ReCiPe 2016 - Midpoint/" + perspectives[i],
-                              indicator=sheet.name,
+                              indicator=sheet.title,
                               indicator_unit=indicator_unit,
-                              flow=xls.cell_str(sheet, row, flow_col),
+                              flow=xls.cell_str(row[flow_col]),
                               flow_category=compartment,
                               flow_unit=flow_unit,
                               cas_number=cas,
                               factor=val)
                 factor_count += 1
         else:
-            val = xls.cell_f64(sheet, row, data_col)
+            val = xls.cell_f64(row[data_col])
             if val == 0.0:
                 continue
             for p in perspectives:
                 dfutil.record(records,
                               method="ReCiPe 2016 - Midpoint/" + p,
-                              indicator=sheet.name,
+                              indicator=sheet.title,
                               indicator_unit=indicator_unit,
-                              flow=xls.cell_str(sheet, row, flow_col),
+                              flow=xls.cell_str(row[flow_col]),
                               flow_category=compartment,
                               flow_unit=flow_unit,
                               cas_number=cas,
@@ -248,47 +248,50 @@ def _read_mid_points(sheet: openpyxl.worksheet.worksheet.Worksheet,
     log.debug("extracted %i factors", factor_count)
 
 
-def _find_data_start(sheet: xlrd.book.sheet) -> (int, int, bool):
-    for row, col in xls.iter_cells(sheet):
-        s = xls.cell_str(sheet, row, col)
-        if s is None or s == "":
-            continue
-        if _eqstr(s, "I") or _containstr(s, "Individualist") or _containstr(s, "Individualistic"):
-            return row + 1, col, True
-        if _eqstr(s, "all perspectives"):
-            return row + 1, col, False
+def _find_data_start(sheet: openpyxl.worksheet.worksheet.Worksheet) -> (int, int, bool):
+    for row in sheet.iter_rows():
+        for cell in row:
+            s = xls.cell_str(cell)
+            if s is None or s == "":
+                continue
+            if _eqstr(s, "I") or _containstr(s, "Individualist") or _containstr(s, "Individualistic"):
+                return cell.row + 1, cell.column - 1, True
+            if _eqstr(s, "all perspectives"):
+                return cell.row + 1, cell.column - 1, False
     return -1, -1
 
 
-def _find_flow_column(sheet: xlrd.book.sheet) -> int:
-    if _containstr(sheet.name, "land", "occupation"):
+def _find_flow_column(sheet: openpyxl.worksheet.worksheet.Worksheet) -> int:
+    if _containstr(sheet.title, "land", "occupation"):
         ncol = 1
         return ncol
     ncol = -1
-    for row, col in xls.iter_cells(sheet):
-        s = xls.cell_str(sheet, row, col)
-        if _containstr(s, "name") or _containstr(s, "substance"):
-            ncol = col
-            log.debug("identified column %i %s for flow names", ncol, s)
-            break
+    for row in sheet.iter_rows():
+        for cell in row:
+            s = xls.cell_str(cell)
+            if _containstr(s, "name") or _containstr(s, "substance"):
+                ncol = cell.column - 1
+                log.debug("identified column %i %s for flow names", ncol, s)
+                break
     if ncol < 0:
-        log.debug("no 'name' column in %s, take col=0 for that", sheet.name)
+        log.debug("no 'name' column in %s, take col=0 for that", sheet.title)
         ncol = 0
     return ncol
 
 
-def _find_cas_column(sheet: xlrd.book.sheet) -> int:
+def _find_cas_column(sheet: openpyxl.worksheet.worksheet.Worksheet) -> int:
     ccol = -1
-    for row, col in xls.iter_cells(sheet):
-        s = xls.cell_str(sheet, row, col)
-        if _eqstr(s, "cas"):
-            ccol = col
-            log.debug("identified column %i %s for CAS numbers", ccol, s)
-            break
+    for row in sheet.iter_rows():
+        for cell in row:
+            s = xls.cell_str(cell)
+            if _eqstr(s, "cas"):
+                ccol = cell.column - 1
+                log.debug("identified column %i %s for CAS numbers", ccol, s)
+                break
     return ccol
 
 
-def _determine_units(sheet: xlrd.book.sheet) -> (str, str, int):
+def _determine_units(sheet: openpyxl.worksheet.worksheet.Worksheet) -> (str, str, int):
     indicator_unit = "?"
     flow_unit = "?"
     unit_col = -1
@@ -296,7 +299,7 @@ def _determine_units(sheet: xlrd.book.sheet) -> (str, str, int):
     row -= 2
 
     if row > 0:
-        s = xls.cell_str(sheet, row, col)
+        s = xls.cell_str(sheet.cell(row=row, column = col + 1))
         if s is not None and s != "":
             if "/" in s:
                 parts = s.strip(" ()").split("/")
@@ -305,23 +308,24 @@ def _determine_units(sheet: xlrd.book.sheet) -> (str, str, int):
             else:
                 indicator_unit = s.strip()
 
-    for row, col in xls.iter_cells(sheet):
-        if row > 5:
+    for count, row in enumerate(sheet.iter_rows()):
+        if count > 5:
             break
-        s = xls.cell_str(sheet, row, col)
-        if _eqstr(s, "Unit"):
-            unit_col = col
-            break
+        for cell in row:
+            s = xls.cell_str(cell)
+            if _eqstr(s, "Unit"):
+                unit_col = cell.column - 1
+                break
 
     if indicator_unit != "?":
         log.debug("determined indicator unit: %s", indicator_unit)
-    elif _containstr(sheet.name, "land", "transformation"):
+    elif _containstr(sheet.title, "land", "transformation"):
         log.debug("unknown indicator unit; assuming it is m2")
         indicator_unit = "m2"
-    elif _containstr(sheet.name, "land", "occupation"):
+    elif _containstr(sheet.title, "land", "occupation"):
         log.debug("unknown indicator unit; assuming it is m2*a")
         indicator_unit = "m2*a"
-    elif _containstr(sheet.name, "water", "consumption"):
+    elif _containstr(sheet.title, "water", "consumption"):
         log.debug("unknown indicator unit; assuming it is m3")
         indicator_unit = "m3"
     else:
@@ -334,13 +338,13 @@ def _determine_units(sheet: xlrd.book.sheet) -> (str, str, int):
         log.debug("take units from column %i", unit_col)
     elif flow_unit != "?":
         log.debug("determined flow unit: %s", flow_unit)
-    elif _containstr(sheet.name, "land", "transformation"):
+    elif _containstr(sheet.title, "land", "transformation"):
         log.debug("unknown flow unit; assume it is m2")
         flow_unit = "m2"
-    elif _containstr(sheet.name, "land", "occupation"):
+    elif _containstr(sheet.title, "land", "occupation"):
         log.debug("unknown flow unit; assuming it is m2*a")
         flow_unit = "m2*a"
-    elif _containstr(sheet.name, "water", "consumption"):
+    elif _containstr(sheet.title, "water", "consumption"):
         log.debug("unknown flow unit; assuming it is m3")
         flow_unit = "m3"
     else:
@@ -350,37 +354,38 @@ def _determine_units(sheet: xlrd.book.sheet) -> (str, str, int):
     return indicator_unit, flow_unit, unit_col
 
 
-def _determine_compartments(sheet: xlrd.book.sheet) -> (str, int):
+def _determine_compartments(sheet: openpyxl.worksheet.worksheet.Worksheet) -> (str, int):
     compartment_col = -1
-    for row, col in xls.iter_cells(sheet):
-        if row > 5:
+    for count, row in enumerate(sheet.iter_rows()):
+        if count > 5:
             break
-        s = xls.cell_str(sheet, row, col)
-        if _containstr(s, "compartment") \
-            or _containstr(s, "name", "in", "ReCiPe"):
-            compartment_col = col
-            break
+        for cell in row:
+            s = xls.cell_str(cell)
+            if _containstr(s, "compartment") \
+                or _containstr(s, "name", "in", "ReCiPe"):
+                compartment_col = cell.column - 1
+                break
 
     if compartment_col > -1:
         log.debug("found compartment column %i", compartment_col)
         return "", compartment_col
 
-    elif _containstr(sheet.name, "global", "warming") \
-            or _containstr(sheet.name, "ozone") \
-            or _containstr(sheet.name, "particulate") \
-            or _containstr(sheet.name, "acidification"):
+    elif _containstr(sheet.title, "global", "warming") \
+            or _containstr(sheet.title, "ozone") \
+            or _containstr(sheet.title, "particulate") \
+            or _containstr(sheet.title, "acidification"):
         log.debug("no compartment column; assuming 'air'")
         return "air", -1
 
-    elif _containstr(sheet.name, "mineral", "resource", "scarcity"):
+    elif _containstr(sheet.title, "mineral", "resource", "scarcity"):
         log.debug("no compartment column; assuming 'resource/ground'")
         return "resource/ground", -1
 
-    elif _containstr(sheet.name, "fossil", "resource", "scarcity"):
+    elif _containstr(sheet.title, "fossil", "resource", "scarcity"):
         log.debug("no compartment column; assuming 'resource'")
         return "resource", -1
 
-    if _containstr(sheet.name, "water", "consumption"):
+    if _containstr(sheet.title, "water", "consumption"):
         log.debug("no compartment column; assuming 'resource/fresh water'")
         return "resource/fresh water", -1
 
