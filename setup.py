6fc71bcb31f1fb2cde88ff271341feb879c677fa
Ben Young <Ben.Young@erg.com>
2021-10-21 15:24:47 -0400

    - update version to 1.0.3 - revise custom naming of stored methods

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lciafmt",
-    version="1.0.1",
+    version="1.0.3",
     packages=["lciafmt"],
     package_dir={'lciafmt': 'lciafmt'},
     package_data={'lciafmt': ["data/*.*"]},
