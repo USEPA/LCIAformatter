e578386b083f355339e730bffb3f80ef89ecb4e2
Ben Young <Ben.Young@erg.com>
2022-08-10 19:27:47 -0400

    parse ipcc file

--- lciafmt/ipcc.py
+++ lciafmt/ipcc.py
@@ -7,6 +7,7 @@ This module contains functions needed to compile LCIA methods from IPCC
 
 import pandas as pd
 from lciafmt.util import log, datapath
+from lciafmt.df import lciafmt_cols
 
 
 def get() -> pd.DataFrame:
@@ -15,9 +16,27 @@ def get() -> pd.DataFrame:
     """
     log.info("get method IPCC")
 
-    filename = ''
+    filename = 'IPCC_GWP_values.csv'
     df = pd.read_csv(datapath + filename)
+    df['Indicator'] = df['AR'] + '-' + df['Parameter'].str.replace('GWP', '')
+    df['Method'] = 'IPCC'
+    df['Indicator unit'] = 'kg CO2 eq'
+    df['Context'] = 'air'
+    df['Unit'] = 'kg'
+    df = df[df['Value'] != '(no data)']
+    df = df.dropna(subset=['Name'])
+    df['Value'] = df['Value'].astype('float')
+    df = df.rename(columns={'Name': 'Flowable',
+                            'CAS': 'CAS No',
+                            'Value': 'Characterization Factor'})
 
-
+    df = df.reindex(columns=lciafmt_cols)
 
     return df
+
+
+if __name__ == "__main__":
+    import lciafmt
+    method = lciafmt.Method.IPCC
+    df = lciafmt.get_method(method)
+    mapped_df = lciafmt.map_flows(df, system=method.get_metadata().get('mapping'))
