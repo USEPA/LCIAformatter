44781b60d793cfd21af4ef243accb215b229ed5f
Ben Young <Ben.Young@erg.com>
2024-04-17 15:23:47 -0400

    add function to extract geocoordinates from ecoinvent source (used by openLCA)

---
+++ lciafmt/location.py
@@ -0,0 +1,36 @@
+# location.py (lciafmt)
+# !/usr/bin/env python3
+# coding=utf-8
+"""
+Functions to facilitate creation of location objects
+"""
+
+import bz2
+import json
+
+from esupy.remote import make_url_request
+
+# %% get GeoJSON
+url = 'https://geography.ecoinvent.org/files'
+
+object_dict = {'states': 'states.geojson.bz2'}
+
+def extract_coordinates(group) -> dict:
+    file = object_dict.get(group)
+    if file is None:
+        print('error')
+        return
+    response = make_url_request(f'{url}/{file}')
+    content = bz2.decompress(response.content)
+    data = json.loads(content)
+
+    # %% extract GeoJSON objects from the FeatureCollection
+    features = data['features']
+   
+    us_states = {f['properties']['shortname']: f['geometry']
+                 for f in features 
+                 if f['properties']['shortname'].startswith('US')}
+    return us_states
+
+if __name__ == "__main__":
+    d = extract_coordinates(group='states')
