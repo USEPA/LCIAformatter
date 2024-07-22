20b43ef0ea14faee6c9ecbeb74f8d19fb104b435
Ben Young <ben.young@erg.com>
2024-07-22 11:45:06 -0400

    initiate config for non-lcia factors

---
+++ scripts/ISO21930-NonLCIA-US.py
@@ -0,0 +1,20 @@
+"""
+Generate method for Non-LCIA Factors per ISO21930
+"""
+
+import lciafmt
+import yaml
+
+m = 'ISO21930-NonLCIA-US.yaml'
+with open(lciafmt.util.datapath / m) as f:
+    file = yaml.safe_load(f)
+version = file.get('version')
+
+df = lciafmt.custom.generate_lcia_compilation(m)
+name = df['Method'][0]
+if version:
+    name = f'{name}{version}'
+
+lciafmt.util.store_method(df, method_id=None, name=name)
+lciafmt.util.save_json(method_id=None, mapped_data=df, name=name)
+df.to_excel(f'{lciafmt.util.OUTPUTPATH}/{name}.xlsx', index=False)
