18a179f9dc0dcf142f448caf43a892dd92eaaecb
Ben Young <ben.young@erg.com>
2024-11-13 13:29:04 -0500

    update ced method and add metadata

--- lciafmt/ced.py
+++ lciafmt/ced.py
@@ -14,21 +14,23 @@ from lciafmt.util import store_method
 
 def get() -> pd.DataFrame():
 
-    inv_orig = lciafmt.get_method(method_id = 'FEDEFL_INV', subset = ['energy'])
+    inv_orig = lciafmt.get_method(method_id = 'FEDEFL_INV', subset = ['ced'])
     inv = inv_orig.copy()
     inv['Indicator'] = ''
     inv['Method'] = 'Cumulative Energy Demand'
 
     conditions = [
+        inv['Flowable'].isin(['Wood, primary forest']),
         inv['Flowable'].isin(['Biomass', 'Softwood', 'Hardwood', 'Wood']),
         inv['Flowable'].isin(['Energy, hydro']),
         inv['Flowable'].str.contains('|'.join(['wind', 'solar', 'geothermal'])),
         inv['Flowable'].str.contains('Uranium'),
         inv['Flowable'].str.contains('|'.join(['Coal', 'Oil', 'Crude',
-                                               'gas'])),
+                                               'gas', 'Methane'])),
         ]
 
-    indicators = ['Renewable, biomass',
+    indicators = ['Non-renewable, biomass',
+                  'Renewable, biomass',
                   'Renewable, water',
                   'Renewable, wind, solar, geothermal',
                   'Non-renewable, nuclear',
@@ -41,8 +43,9 @@ def get() -> pd.DataFrame():
     # https://www.lcacommons.gov/lca-collaboration/National_Renewable_Energy_Laboratory/USLCI_Database_Public/dataset/FLOW/46dc4693-2f24-39d2-b69f-dd059737fd5e
 
     ## Original CED Method used HHV for biomass/wood flows
+    # Some wood flows were removed after the original method in FEDEFLv1.0.8
 
-    # Dropped flows: "Hydrogen", "Energy, heat"
+    # Dropped flows from FEDEFL inv method: "Hydrogen", "Energy, heat"
 
     inv = inv.query('Indicator != ""').reset_index(drop=True)
 
